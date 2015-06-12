7b9c8f6ef8232694c3d845b23e6c6ec648771871
Tianci Zhong <tzhong@brandeis.edu>
2015-06-12 17:16:02 -0400

    add more location

--- server/lib/locations.js
+++ server/lib/locations.js
@@ -23,7 +23,43 @@ locations = [
 		"function": "The SCC is home to many of Brandeis' media clubs, the bookstore, and the Student Union. Students can access the SCC 24/7 and many can be found studying here.",
 		"description":"green",
 		"category":"studentCenter"
-	}
+	},
+	fellowsgarden = {
+		"name": "Fellows Garden",
+		"nickname": "Fellows Garden",
+		"coordinates": [
+			new Point(42.366813,-71.259628),
+			new Point(42.366375,-71.260151),
+			new Point(42.365813,-71.259543),
+			new Point(42.366023,-71.259101)],
+		"function": "Fellows Garden is a nice place to hangout with friends in nice weather",
+		"description":"Nobody knowns the name",
+		"category":"Lawn"
+	},
+	rosen = {
+		"name": "The Rose Art Museum",
+		"nickname": "Rosen",
+		"coordinates": [
+			new Point(42.366033,-71.262631),
+			new Point(42.365498,-71.262948),
+			new Point(42.365442,-71.262288),
+			new Point(42.365902,-71.262218)],
+		"function": "The Rose Art Museum",
+		"description":"The Museum",
+		"category":"Other"
+	},
+	bassine = {
+		"name": "Bassine Science Building",
+		"nickname": "Bassine",
+		"coordinates": [
+			new Point(42.367122,-71.258208),
+			new Point(42.366997,-71.258307),
+			new Point(42.366782,-71.257457),
+			new Point(42.366656,-71.257562)],
+		"function": "Bassine Science Building has many bio lab?",
+		"description":"Lab",
+		"category":"Classroom"
+	},
 ]
 
 function Point(x,y) {
