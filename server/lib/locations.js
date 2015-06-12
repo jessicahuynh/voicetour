4bc91ab4cd65e38c4933d9d4869b24c9ed6cc637
Jessica Huynh <jhuynh37@brandeis.edu>
2015-06-12 14:32:56 -0400

    got locations json file to load into collection!

---
+++ server/lib/locations.js
@@ -0,0 +1,13 @@
+locations = [
+	volen = {
+		"name": "The Volen National Center for Complex Systems",
+		"nickname": "Volen",
+		"coordinates": [],
+		"function": "Volen is home to Brandeis' computer science and linguistics departments.",
+		"description": "A longer description",
+
+	},
+	gzang = {
+		"name":"Gerstenzang"
+	}
+]
---
+++ server/startup.js
@@ -0,0 +1,8 @@
+Meteor.startup(function() {
+	if (Locations.find().count() == 0) {
+		// load locations json file into collection
+		locations.forEach(function(location) {
+			Locations.insert(location);
+		});
+	}
+});
