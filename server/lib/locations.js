deff1dda4b7dc04bba8fd37a44b9902dbf4740d4
Andrea <ziyuqiu@brandeis.edu>
2015-06-16 16:00:31 -0400

    GPS locations update1

--- server/lib/locations.js
+++ server/lib/locations.js
@@ -36,7 +36,7 @@ locations = [
 		"description":"Nobody knows the name",
 		"category":"Lawn"
 	},
-	rose = {
+	museum = {
 		"name": "The Rose Art Museum",
 		"nickname": "Rose",
 		"coordinates": [
@@ -72,6 +72,59 @@ locations = [
 		"description":"stuff",
 		"category":"Offices"
 	}
+
+turner={"name":"60 Turner Street","coordinates":[new Point(42.362028,-71.264118), new Point(42.361976,-71.263828),new Point(42.361834,-71.26386),new Point(42.361889,-71.264187)]},
+567={"name":"567 South Street Apartments","coordinates":[new Point(42.36141,-71.261572), new Point(42.361333,-71.261402), new Point(42.361165,-71.261552), new Point(42.36124,-71.261714)]},
+bernstein={"name":"Bernstein-Marcus Administration Center","coordinates":[new Point(42.365077,-71.25998), new Point(42.365222,-71.259642), new Point(42.365046,-71.259492), new Point(42.36489,-71.259837)]},
+coffman={"name":"Coffman Residence Hall","coordinates":[new Point(42.359936,-71.257469), new Point(42.359989,-71.257373), new Point(42.359397,-71.25678), new Point(42.359327,-71.256919)]},
+cohen={"name":"Cohen Residence Hall","coordinates":[new Point(42.359859,-71.257805), new Point(42.359894,-71.257695), new Point(42.359111,-71.257282), new Point(42.359074,-71.257394)]},
+epstein={"name":"Epstein Building","coordinates":[new Point(42.362457,-71.261636), new Point(42.362374,-71.260823), new Point(42.362134,-71.260864), new Point(42.362204,-71.261671)]},
+facultyCenter={"name":"Faculty Center","coordinates":[new Point(42.36569,-71.261466), new Point(42.365861,-71.261069), new Point(42.365651,-71.260898), new Point(42.36548,-71.2613)]},
+goldman={"name":"Goldman-Schwartz Fine Arts","coordinates":[new Point(42.365001,-71.263157), new Point(42.364799,-71.26258), new Point(42.364525,-71.262821), new Point(42.364628,-71.263304)]},
+gryzmish={"name":"Gryzmish Center","coordinates":[new Point(42.365593,-71.259553), new Point(42.365759,-71.25918), new Point(42.365577,-71.259025), new Point(42.365417,-71.259403)]},
+sachar={"name":"Sachar International Center","coordinates":[new Point(42.365064,-71.25997), new Point(42.365424,-71.259121), new Point(42.365279,-71.258993), new Point(42.364897,-71.25983)]},
+irving={"name":"Irving Presidential Enclave","coordinates":[new Point(42.365209,-71.259623), new Point(42.365423,-71.259113), new Point(42.36529,-71.259006), new Point(42.365076,-71.259521)]},
+lewis={"name":"Lewis Residence Hall","coordinates":[new Point(42.360203,-71.257102), new Point(42.359815,-71.256402), new Point(42.359742,-71.25648), new Point(42.36013,-71.257177)]},
+info={"name":"Information Booth","coordinates":[new Point(42.365015,-71.258721), new Point(42.365046,-71.258676), new Point(42.364978,-71.258681), new Point(42.365007,-71.258634)]},
+may={"name":"May Residence Hall","coordinates":[new Point(42.360515,-71.25694), new Point(42.360223,-71.256167), new Point(42.36015,-71.256216), new Point(42.360431,-71.256994)]},
+mazer={"name":"Mazer Residence Hall","coordinates":[new Point(42.36312,-71.261151), new Point(42.363011,-71.260811), new Point(42.362781,-71.260933), new Point(42.362912,-71.261273)]},
+pollack={"name":"Pollack Fine Arts Teaching Center","coordinates":[new Point(42.365328,-71.26243), new Point(42.365291,-71.262325), new Point(42.365122,-71.262435), new Point(42.365158,-71.262541)]},
+rabb={"name":"The Rabb School","coordinates":[new Point(42.36298,-71.25929), new Point(42.363061,-71.258924), new Point(42.362961,-71.258887), new Point(42.362872,-71.259233)]},
+ridgewoodA={"name":"Ridgewood Residence Hall A","coordinates":[new Point(42.364163,-71.260638), new Point(42.364036,-71.260324), new Point(42.363893,-71.260338), new Point(42.364028,-71.260729)]},
+ridgewoodB={"name":"Ridgewood Residence Hall B","coordinates":[new Point(42.363939,-71.261456), new Point(42.363978,-71.260872), new Point(42.363844,-71.260957), new Point(42.363788,-71.261432)]},
+ridgewoodC={"name":"Ridgewood Residence Hall C","coordinates":[new Point(42.363691,-71.260904), new Point(42.363562,-71.260466), new Point(42.363425,-71.260566), new Point(42.363584,-71.261043)]},
+rosyEast={"name":"Rosenthal Residence Hall East","coordinates":[new Point(42.367225,-71.260011), new Point(42.367225,-71.259817), new Point(42.366945,-71.25979), new Point(42.366962,-71.259988)]},
+rosyNorth={"name":"Rosenthal Residence Hall North","coordinates":[new Point(42.367388,-71.26032), new Point(42.367364,-71.259949), new Point(42.367263,-71.25996), new Point(42.367268,-71.260348)]},
+rosySouth={"name":"Rosenthal Residence Hall South","coordinates":[new Point(42.366942,-71.260352), new Point(42.366919,-71.25997), new Point(42.366814,-71.259979), new Point(42.36681,-71.260375)]},
+admission={"name":"Carl and Ruth Shapiro Adimission Center","coordinates":[new Point(42.36615,-71.260189), new Point(42.365803,-71.259658), new Point(42.365546,-71.259934), new Point(42.36552,-71.260634)]},
+shapiro={"name":"Shapiro Residence Hall","coordinates":[new Point(42.367058,-71.261655), new Point(42.367139,-71.261459), new Point(42.366469,-71.26103), new Point(42.366398,-71.261164)]},
+renfield={"name":"Renfield Residence Hall","coordinates":[new Point(42.367591,-71.261234), new Point(42.367458,-71.262894), new Point(42.367373,-71.260839), new Point(42.367498,-71.261277)]},
+deroy={"name":"Deroy Residence Hall","coordinates":[new Point(42.367526,-71.261539), new Point(42.367458,-71.261429), new Point(42.367238,-71.261668), new Point(42.3673,-71.261778)]},
+usen={"name":"Usen Residence Hall","coordinates":[new Point(42.367359,-71.260625), new Point(42.367351,-71.260477), new Point(42.367,-71.260499), new Point(42.367012,-71.26066)]},
+slosberg={"name":"Slosberg Music Center","coordinates":[new Point(42.364412,-71.259683), new Point(42.364319,-71.2594), new Point(42.363891,-71.259666), new Point(42.363985,-71.259942)]},
+spingold={"name":"Spingold Theater Center","coordinates":[new Point(42.364788,-71.262031), new Point(42.364612,-71.261474), new Point(42.364008,-71.261774), new Point(42.364194,-71.262415)]},
+villiage={"name":"Villiage Residence Hall","coordinates":[new Point(42.363835,-71.260165), new Point(42.363763,-71.259809), new Point(42.362819,-71.260465), new Point(42.362911,-71.260755)]},
+zivA={"name":"Ziv Residence Hall A","coordinates":[new Point(42.363537,-71.261639), new Point(42.363459,-71.261421), new Point(42.363227,-71.261566), new Point(42.363294,-71.261831)]},
+zivB={"name":"Ziv Residence Hall B","coordinates":[new Point(42.363197,-71.261606), new Point(42.363108,-71.261305), new Point(42.362904,-71.261391), new Point(42.363009,-71.26174)]},
+zivD={"name":"Ziv Residence Hall D","coordinates":[new Point(42.363419,-71.261034), new Point(42.36331,-71.260694), new Point(42.363126,-71.260815), new Point(42.363215,-71.261112)]},
+Abelson={"name":"Abelson-Bass-Yalem","coordinates":[new Point(42.366696,-71.258464), new Point(42.366554,-71.258437), new Point(42.366157,-71.258726), new Point(42.366226,-71.258882)]},
+berlin={"name":"Berlin Chapel","coordinates":[new Point(42.368284,-71.260708), new Point(42.368244,-71.260593), new Point(42.368082,-71.260705), new Point(42.368135,-71.26081)]},
+bethlehem={"name":"Bethlehem Chapel","coordinates":[new Point(42.368343,-71.260178), new Point(42.368259,-71.259998), new Point(42.368196,-71.260047), new Point(42.368267,-71.260243)]},
+harlan={"name":"Harlan Chapel","coordinates":[new Point(42.368776,-71.260739), new Point(42.368692,-71.260564), new Point(42.36865,-71.2606), new Point(42.368732,-71.260779)]},
+chapelField={"name":"Chapel Fields","coordinates":[new Point(42.368103,-71.260244), new Point(42.36763,-71.25901), new Point(42.367281,-71.259034), new Point(42.367681,-71.260542)]},
+brwon={"name":"Brown Social Science Center","coordinates":[new Point(42.367383,-71.257225), new Point(42.36728,-71.256666), new Point(42.367169,-71.256718), new Point(42.367266-71.257265,)]},
+edison={"name":"Edison-Lecks Science Building","coordinates":[new Point(42.36652,-71.25835), new Point(42.366312,-71.25788), new Point(42.366168,-71.258001), new Point(42.366374,-71.258462)]},
+farber={"name":"Farber Library","coordinates":[new Point(42.367907,-71.258866), new Point(42.368046,-71.25855), new Point(42.367823,-71.258354), new Point(42.367688,-71.25867)]},
+goldsmith={"name":"Goldsmith Building","coordinates":[new Point(42.367263,-71.258512), new Point(42.365328,-71.26243), new Point(42.367122,-71.258208), new Point(42.366997,-71.258307)]},
+heller={"name":"Heller-Brown Building","coordinates":[new Point(42.369326,-71.259107), new Point(42.369187,-71.258417), new Point(42.369025,-71.258474), new Point(42.369158,-71.259171)]},
+rabbGrad={"name":"Rabb Graduate Center","coordinates":[new Point(42.369289,-71.25762), new Point(42.369127,-71.256792), new Point(42.36915,-71.257674), new Point(42.368996,-71.256845)]},
+roseMedical={"name":"Rosenstiel Basic Medical Sciences Research Center","coordinates":[new Point(42.366103,-71.257429), new Point(42.365901,-71.257013), new Point(42.365724,-71.257239), new Point(42.365884,-71.257585)]},
+schwartz={"name":"Schwartz Hall","coordinates":[new Point(42.367642,-71.257438), new Point(42.367563,-71.257024), new Point(42.367418,-71.257072), new Point(42.367494,-71.257484)]},
+SSC={"name":"Carl J. Shapiro Science Center","coordinates":[new Point(42.36595,-71.258617), new Point(42.366115,-71.257734), new Point(42.365759,-71.258559), new Point(42.365894,-71.257864)]},
+usdan={"name":"Usdan Student Center","coordinates":[new Point(42.368296,-71.256585), new Point(42.367852,-71.256794), new Point(42.367777,-71.256451), new Point(42.368243,)]},
+gosman={"name":"Gosman Sports and Convocation Center","coordinates":[new Point(42.365193,-71.255688), new Point(42.365761,-71.25482), new Point(42.364998,-71.253952), new Point(42.364449,)]}
+
+		
 ]
 
 function Point(x,y) {
