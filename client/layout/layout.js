3830413e51c009012188c32ffd895893f4ee312e
Jessica Huynh <jhuynh37@brandeis.edu>
2015-06-19 14:54:34 -0400

    got category dropdown working

--- client/layout/layout.js
+++ client/layout/layout.js
@@ -4,6 +4,15 @@ Template.layout.events({
 		var s = searchBox.value;
 		Session.set("searchTerm",s);
 		
-		Router.go('/search');
+		Router.go('/search/'+Session.get("searchTerm"));
+	},
+	// changes category in the dropdown
+	'click #selectSearchType li a':function(event) {
+		event.preventDefault();
+		$(event.target).parents('.btn-group').find('.btn').text($(event.target).text());
+  		$(event.target).parents('.btn-group').find('.btn').val($(event.target).text());
+
+  		// append the caret
+  		$(event.target).parents('.btn-group').find('.btn').append('&nbsp;<span class="caret"></span>');
 	}
 });
