5b11c597f97b2a4aa1d5ec2a8509952d5cb91253
Jessica Huynh <jhuynh37@brandeis.edu>
2015-06-26 15:01:55 -0400

    filling info page

--- client/locationListing.js
+++ client/locationListing.js
@@ -1,6 +1,15 @@
 Template.locationListing.helpers({
 	locationId:function() {
 		return this._id;
+	},
+	categories:function() {
+		var c = "";
+		var catArray = this.category;
+		for (var i = 0; i < catArray.length - 1; i++) {
+			c += catArray[i] + ", ";
+		}
+		c+=catArray[i];
+		return c;
 	}
 });
 
