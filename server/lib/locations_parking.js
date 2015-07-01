f7c469b72ae706238efa8d4fbad55a157cdd88da
Tianci Zhong <tzhong@brandeis.edu>
2015-07-01 16:10:50 -0400

    gym data

---
+++ server/lib/locations_parking.js
@@ -0,0 +1,36 @@
+locations_parking = [
+	gymp1 = {
+		"name":"Athletics lot",
+		"nickname":"Athletics lot",
+		"coordinates":[
+			new Point(42.365088,-71.256173), 
+			new Point(42.364914,-71.256057), 
+			new Point(42.364422,-71.257879), 
+			new Point(42.364272,-71.257766)],
+		"category":["parking"]
+	},
+	gymp2 = {
+		"name":"gosman parking lot",
+		"nickname":"gosman parking lot ",
+		"coordinates":[
+			new Point(42.365056,-71.253832), 
+			new Point(42.364858,-71.253718), 
+			new Point(42.365541,-71.252837), 
+			new Point(42.365393,-71.252676)],
+		"category":["parking"]
+	},
+	gymp3 = {
+		"name":"linsey parking lot",
+		"nickname":"linsey parking lot ",
+		"coordinates":[
+			new Point(42.365568,-71.254114), 
+			new Point(42.365449,-71.253988), 
+			new Point(42.365645,-71.253642), 
+			new Point(42.365739,-71.253857)],
+		"category":["parking"]
+	},
+]
+function Point(x,y) {
+	this.x = x;
+	this.y = y;
+}
---
+++ server/lib/points_gym.js
@@ -0,0 +1,126 @@
+points = [
+	{
+		"id":"gosman_c01",
+		"coordinate":new Point(42.365305,-71.256108),
+		"type":"crossing"
+	},
+	{
+		"id":"gosman_c02",
+		"coordinate":new Point(42.365022,-71.255821),
+		"type":"crossing"
+	},
+	{
+		"id":"Athleticslot_e01",
+		"coordinate":new Point(42.365022,-71.255865),
+		"type":"entrance"
+	},
+	{
+		"id":"gosman_e01",
+		"coordinate":new Point(42.365142,-71.255437),
+		"type":"entrance"
+	},
+	{
+		"id":"gosman_e02",
+		"coordinate":new Point(42.365420,-71.254361),
+		"type":"entrance"
+	},
+	{
+		"id":"Linsey_c02",
+		"coordinate":new Point(42.365553,-71.254355),
+		"type":"crossing"
+	},
+	{
+		"id":"gosman_e03",
+		"coordinate":new Point(42.365513,-71.254510),
+		"type":"entrance"
+	},
+	{
+		"id":"gosman_e04",
+		"coordinate":new Point(42.365747,-71.254770),
+		"type":"entrance"
+	},
+	{
+		"id":"linsey_e01",
+		"coordinate":new Point(42.365826,-71.254011),
+		"type":"entrance"
+	},
+	{
+		"id":"linsey_e02",
+		"coordinate":new Point(42.365625,-71.254379),
+		"type":"entrance"
+	},
+	{
+		"id":"Linsey_c03",
+		"coordinate":new Point(42.365810,-71.253929),
+		"type":"crossing"
+	},
+	{
+		"id":"linseyparkinglot_e01",
+		"coordinate":new Point(42.365682,-71.253661),
+		"type":"entrance"
+	},
+	{
+		"id":"linseyparkinglot_e02",
+		"coordinate":new Point(42.365738,-71.253813),
+		"type":"entrance"
+	},
+	{
+		"id":"gosmanparkinglot_e01",
+		"coordinate":new Point(42.364890,-71.253780),
+		"type":"entrance"
+	},
+	{
+		"id":"gosmanparkinglot_e02",
+		"coordinate":new Point(42.365017,-71.253815),
+		"type":"entrance"
+	},
+	{
+		"id":"Linsey_c01",
+		"coordinate":new Point(42.365969,-71.254845),
+		"type":"crossing"
+	},
+	{
+		"id":"mailman_e01",
+		"coordinate":new Point(42.365801,-71.255926),
+		"type":"entrance"
+	},
+	{
+		"id":"superconducting_e01",
+		"coordinate":new Point(42.365910,-71.255390),
+		"type":"entrance"
+	},
+	{
+		"id":"stoneman_e01",
+		"coordinate":new Point(42.366141,-71.255637),
+		"type":"entrance"
+	},
+	{
+		"id":"healthcenter_e01",
+		"coordinate":new Point(42.366189,-71.255242),
+		"type":"entrance"
+	},
+	{
+		"id":"police_e01",
+		"coordinate":new Point(42.366250,-71.255440),
+		"type":"entrance"
+	},
+	{
+		"id":"castle_e01",
+		"coordinate":new Point(42.367034,-71.255943),
+		"type":"entrance"
+	},
+	{
+		"id":"castle_e02",
+		"coordinate":new Point(42.367427,-71.256041),
+		"type":"entrance"
+	},
+
+
+
+
+]
+
+function Point(x,y) {
+	this.x = x;
+	this.y = y;
+}
