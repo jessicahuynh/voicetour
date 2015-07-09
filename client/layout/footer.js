ea5de8867133715698d10898e3ec9c9a08279712
Jessica Huynh <jhuynh37@brandeis.edu>
2015-07-08 23:25:57 -0400

    don't display mic if browser doesn't support webrtc

--- client/layout/footer.js
+++ client/layout/footer.js
@@ -69,67 +69,72 @@ Template.footer.events({
 });
 
 Template.footer.rendered = function() {
-
-    var mic = new Wit.Microphone(document.getElementById("microphone"));
-    var info = function (msg) {
-        document.getElementById("info").innerHTML = msg;
-    };
-    var error = function (msg) {
-        document.getElementById("error").innerHTML = msg;
-    };
-    mic.onready = function () {
-        info("Microphone is ready to record");
-    };
-    mic.onaudiostart = function () {
-        info("Recording started");
-        error("");
-       
-        $("#speechText").show("slow");
-        $("#result").append("<h4>I'm listening!</h4>")
-    };
-    mic.onaudioend = function () {
-        info("Recording stopped, processing started");
-    };
-    mic.onresult = function (intent, entities) {
-        // console.log(intent + " " + entities);
-        
-        var r = kv("intent", intent);
-
-        for (var k in entities) {
-            var e = entities[k];
-
-            if (!(e instanceof Array)) {
-        r += kv(k, e.value);
-            } else {
-        for (var i = 0; i < e.length; i++) {
-          r += kv(k, e[i].value);
-        }
+    navigator.getUserMedia = (navigator.getUserMedia || 
+                          navigator.webkitGetUserMedia || 
+                          navigator.mozGetUserMedia || 
+                          navigator.msGetUserMedia);
+    if (navigator.getUserMedia) {
+        var mic = new Wit.Microphone(document.getElementById("microphone"));
+        var info = function (msg) {
+            document.getElementById("info").innerHTML = msg;
+        };
+        var error = function (msg) {
+            document.getElementById("error").innerHTML = msg;
+        };
+        mic.onready = function () {
+            info("Microphone is ready to record");
+        };
+        mic.onaudiostart = function () {
+            info("Recording started");
+            error("");
+           
+            $("#speechText").show("slow");
+            $("#result").append("<h4>I'm listening!</h4>")
+        };
+        mic.onaudioend = function () {
+            info("Recording stopped, processing started");
+        };
+        mic.onresult = function (intent, entities) {
+            // console.log(intent + " " + entities);
+            
+            var r = kv("intent", intent);
+    
+            for (var k in entities) {
+                var e = entities[k];
+    
+                if (!(e instanceof Array)) {
+            r += kv(k, e.value);
+                } else {
+            for (var i = 0; i < e.length; i++) {
+              r += kv(k, e[i].value);
             }
+                }
+            }
+    
+            document.getElementById("result").innerHTML = r;
+            
+            applyIntent(intent,entities);
+        };
+        mic.onerror = function (err) {
+            error("Error: " + err);
+        };
+        mic.onconnecting = function () {
+            info("Microphone is connecting");
+        };
+        mic.ondisconnected = function () {
+            info("Microphone is not connected");
+        };
+    
+        mic.connect("ANATOUXNLPGVGPTGWPN7RXQHFYYSPGPP");
+        // mic.start();
+        // mic.stop();
+    
+        function kv(k, v) {
+            if (toString.call(v) !== "[object String]") {
+                v = JSON.stringify(v);
+            }
+            return k + "=" + v + "\n";
         }
-
-        document.getElementById("result").innerHTML = r;
-        
-        applyIntent(intent,entities);
-    };
-    mic.onerror = function (err) {
-        error("Error: " + err);
-    };
-    mic.onconnecting = function () {
-        info("Microphone is connecting");
-    };
-    mic.ondisconnected = function () {
-        info("Microphone is not connected");
-    };
-
-    mic.connect("ANATOUXNLPGVGPTGWPN7RXQHFYYSPGPP");
-    // mic.start();
-    // mic.stop();
-
-    function kv(k, v) {
-        if (toString.call(v) !== "[object String]") {
-            v = JSON.stringify(v);
-        }
-        return k + "=" + v + "\n";
     }
 }
 
