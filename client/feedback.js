371681b771aac90d91c24aa281ad6976d9fb9e15
Jessica Huynh <jhuynh37@brandeis.edu>
2015-07-15 22:29:51 -0400

    layout fixes, added feedback form

---
+++ client/feedback.js
@@ -0,0 +1,23 @@
+Template.feedback.events({
+	'submit #feedbackForm':function(event) {
+		event.preventDefault();
+		$.ajax({
+			url:"https://docs.google.com/a/brandeis.edu/forms/d/1bz7aA7XLWCJQgFFYcDEb_pvRZnR2ysjX2RalSHkMXT4/formResponse",
+				data:{
+					"entry_1215249245": $("#feedbackEmail").val(),
+					"entry_641083817":$("#feedbackType").val(),
+					"entry_38663783":$("#feedbackComment").val()
+				},
+				type:"POST",
+				dataType:"xml"
+		});
+		
+		$(".modal-body").html('<p>Thanks for your feedback!</p>');
+	}
+});
+
+Template.feedback.rendered = function() {
+	$('#feedbackForm').on('shown.bs.modal', function () {
+	  $('#feedbackEmail').focus()
+	});
+}
