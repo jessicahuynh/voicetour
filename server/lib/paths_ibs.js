1cf3630d41b3c22cb0ba5bc017576836e0a3d390
Andrea <ziyuqiu@brandeis.edu>
2015-07-20 10:41:02 -0400

    reverse paths of ibs added

--- server/lib/paths_ibs.js
+++ server/lib/paths_ibs.js
@@ -5,14 +5,29 @@ paths_ibs = [
 	    "description": "Walk along the loop road until you see the art installation made of lights."
 	},
 	{
+	    "start":"lawn_c01",
+	    "end":"scc_c10",
+	    "description": "Walk towards the Great Lawn, which is the opposite way of the statue of lights."
+	},
+	{
 		"start":"lawn_c01",
 		"end":"museum_c01",
 		"description":"Continue walking on the loop road until you see the fire hydrant."
 	},
 	{
 		"start":"museum_c01",
+		"end":"lawn_c01",
+		"description":"Continue walking on the loop road and goes to the front of the statue of lights."
+	},
+	{
+		"start":"museum_c01",
 		"end":"museum_c02",
 		"description":"Walk along the small road and turn left, the Rose Art Museum is on your right."
+	},
+	{
+		"start":"museum_c02",
+		"end":"museum_c01",
+		"description":"Walk towards the left side of the statue of lights, turn right to the loop road."
 	},
 	{
 		"start":"lawn_c01",
@@ -21,8 +36,18 @@ paths_ibs = [
 	},
 	{
 		"start":"museum_c03",
+		"end":"lawn_c01",
+		"description":"Walking along the loop road to the front of the statue of lights."
+	},
+	{
+		"start":"museum_c03",
 		"end":"museum_c02",
 		"description":"Walk on the small road and turn right, the Rose Art Museum is on your left."
+	},
+	{
+		"start":"museum_c02",
+		"end":"museum_c03",
+		"description":"Walk towards the right side of the statue of lights,turn left to the loop road."
 	},
 	{
 		"start":"museum_c02",
@@ -30,13 +55,28 @@ paths_ibs = [
 		"description":"Keep walking to the building with glass walls, that's Rose Art Museum."
 	},
 	{
+		"start":"museum_e01",
+		"end":"museum_c02",
+		"description":"Get off the stairs."
+	},
+	{
 		"start":"museum_c03",
 		"end":"pollack_e01",
 		"description":"Walk along the small road leading to the left, it's right beside the large garbage container."
 	},
 	{
+		"start":"pollack_e01",
+		"end":"museum_c03",
+		"description":"Go straight towards the main road."
+	},
+	{
 		"start":"scc_c10",
 		"end":"theater_c01",
+		"description":"Walk across the grasses."
+	},
+	{
+		"start":"theater_c01",
+		"end":"scc_c10",
 		"description":"Walk across the grasses."
 	},
 	{
@@ -46,8 +86,18 @@ paths_ibs = [
 	},
 	{
 		"start":"art_c01",
+		"end":"theater_c01",
+		"description":"Walk along the road beside Spingold (the round building)."
+	},
+	{
+		"start":"art_c01",
 		"end":"goldman_e01",
 		"description":"The Goldman-Schwartz Fine Arts is right beside the road."
+	},
+	{
+		"start":"goldman_e01",
+		"end":"art_c01",
+		"description":"Get on the road."
 	},
 	{
 		"start":"art_c01",
@@ -55,9 +105,19 @@ paths_ibs = [
 		"description":"The Goldman-Schwartz Fine Arts is right beside the road."
 	},
 	{
+		"start":"goldman_e02",
+		"end":"art_c01",
+		"description":"Get on the road."
+	},
+	{
 		"start":"theaterlot_e01",
 		"end":"spingold_e02",
 		"description":"Walk along the side of parking lot toward to the round building. Your destination is in front of you."
+	},
+	{
+		"start":"spingold_e02",
+		"end":"theaterlot_e01",
+		"description":"Walk along the side of parking lot toward the crossing with three routes."
 	},
 	{
 		"start":"art_c01",
@@ -66,13 +126,28 @@ paths_ibs = [
 	},
 	{
 		"start":"theaterlot_e02",
+		"end":"art_c01",
+		"description":"Go straight along the road."
+	},
+	{
+		"start":"theaterlot_e02",
 		"end":"samuellemberg_e01",
 		"description":"Don't head to the parking lot. Take the other way and keep going and Lemberg will be in front of you."
+	},
+	{
+		"start":"samuellemberg_e01",
+		"end":"theaterlot_e02",
+		"description":"Go straight along the road until you see the parking lot."
 	},
 	{
 		"start":"theaterlot_e02",
 		"end":"theaterlot_e03",
 		"description":"Walk along the side of the parking lot until you see the sign of Brandeis International Business School."
+	},
+		{
+		"start":"theaterlot_e03",
+		"end":"theaterlot_e02",
+		"description":"Walk along the side of the parking lot toward the route with a blue sign leading to the red building inside the woods."
 	},
 	{
 		"start":"theaterlot_e03",
@@ -81,9 +156,102 @@ paths_ibs = [
 	},
 	{
 		"start":"samuellemberg_e02",
+		"end":"theaterlot_e03",
+		"description":"Walk along the road until you see the parking lot."
+	},
+	{
+		"start":"samuellemberg_e02",
 		"end":"sachar_e01",
 		"description":"Keep walking forward, the building with flags from different countries is the Sachar International Center."
 	},
+	{
+		"start":"sachar_e01",
+		"end":"samuellemberg_e02",
+		"description":"turn left and walk along the road."
+	},
+	{
+		"start":"theaterlot_e01",
+		"end":"spingold_e02",
+		"description":"Go towards the round building, which is the Spingold Theater Center."
+	},
+	{
+		"start":"spingold_e02",
+		"end":"theaterlot_e01",
+		"description":"Walk towards the intersection with the level crossing."
+	},	
+	{
+		"start":"theater_c01",
+		"end":"spingold_e01",
+		"description":"Go towards to the round building. That's the Spingold Theater Center."
+	},	
+	{
+		"start":"spingold_e01",
+		"end":"theater_c01",
+		"description":"Walk along the road in front of the Spingold Theater Center."
+	},	
+	{
+		"start":"lawn_c02",
+		"end":"spingold_e01",
+		"description":"Go towards the round building along the curving path. That's the Spingold Theater Center."
+	},	
+	{
+		"start":"spingold_e01",
+		"end":"lawn_c02",
+		"description":"Go towards the loop road."
+	},
+	{
+		"start":"theaterlot_e02",
+		"end":"spingold_e02",
+		"description":"Go towards the round building. That's the Spingold Theater Center."
+	},
+	{
+		"start":"spingold_e02",
+		"end":"theaterlot_e02",
+		"description":"Walk along the side of the parking lot until you see the intersection with a blue sign leading to the Goldman-Schwartz Fine Arts."
+	},
+
+
+	{
+		"start":"lawn_c02",
+		"end":"scc_c10",
+		"description":"Walk along the loop road."
+	},
+	{
+		"start":"scc_c10",
+		"end":"lawn_c02",
+		"description":"Walk along the loop road."
+	},
+	{
+		"start":"lawn_c02",
+		"end":"scc_c08",
+		"description":"Walk along the loop road towards the bus stop near admission office."
+	},
+	{
+		"start":"scc_c08",
+		"end":"lawn_c02",
+		"description":"Walk along the loop road."
+	},	
+	{
+		"start":"museum_c03",
+		"end":"theater_c01",
+		"description":"Head to the direction of the round building (Spingold) to the crossing."
+	},
+	{
+		"start":"theater_c01",
+		"end":"museum_c03",
+		"description":"Walk along the small road."
+	},
+	// {
+	// 	"start":"theaterlot_e01",
+	// 	"end":"villiage_c03",
+	// 	"description":"Head to the direction of level crossing go straight along the road."
+	// },
+	// 	{
+	// 	"start":"",
+	// 	"end":"",
+	// 	"description":
+	// },	
+	
 
 
 ]
