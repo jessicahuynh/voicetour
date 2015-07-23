3b0d22a55419063d3ef333feacd490081613e15c
Jessica Huynh <jhuynh37@brandeis.edu>
2015-07-23 18:19:08 -0400

    navigation with full name autocomplete on welcome page

--- client/welcome.js
+++ client/welcome.js
@@ -5,11 +5,6 @@ offCampus = {
 }
 
 Template.welcome.helpers({
-
-	currentLocation: function () {
-		return Session.get("currentLocation");
-
-	},
 	inLocation: function () {
 		var name = Locations.findOne({ "name": Session.get("inLocation")[0].name }).name;
 		if (Session.get("inLocation")[1] == "in") {
@@ -28,15 +23,6 @@ Template.welcome.helpers({
 			return "You're off campus";
 		}
 	},
-	inLocationFunction:function() {
-		var loc = Locations.findOne({"name":Session.get("inLocation")[0].name}); 
-		if (loc == null) {
-			return offCampus.function;
-		}
-		else {
-			return loc.function;
-		}
-	},
 	locationMapOptions: function() {
 		if (GoogleMaps.loaded()) {
 			//console.log(Session.get("currentLocation").x,Session.get("currentLocation").y);
@@ -46,11 +32,24 @@ Template.welcome.helpers({
 			}
 			
 		}
-	}
+	},
+	settings:function() {
+		return {
+			position:"bottom",
+			limit:10,
+			rules:[{
+				collection:Locations,
+				matchAll:true,
+				field:"name",
+				template:Template.suggestions
+			}]
+		};
+	},
 });
 
 Template.welcome.rendered = function() {
 	Session.set("pageTitle","Discover Deis");
+	Session.set("listenTo","Welcome to Discover Deis! You can navigate to any location on campus here and find out more.");
 	
 	GoogleMaps.load();	
 	
@@ -71,63 +70,10 @@ Template.welcome.rendered = function() {
 }
 
 Template.welcome.events({
-	'click #whereAmI': function(event) {
+	'submit #navform':function(event) {
 		event.preventDefault();
-
-		navigator.geolocation.getCurrentPosition(function(position) {
-			var current = new Point(position.coords.latitude,position.coords.longitude);
-			Session.set("currentLocation",current);
-		});
 		
-
-		Meteor.call("searchLocations",			
-			Session.get("currentLocation"),
-			function(error, data) {
-				if (error) {
-					console.log(error);
-				}
-				else {
-					Session.set("inLocation",data);
-				}
-			}
-			);
-
-		if (Session.get("inLocation") == null) {
-			window.speechSynthesis.speak(new SpeechSynthesisUtterance("You are off campus."));
-		}
-		else {
-			var name = Session.get("inLocation")[0].name;
-
-			if (Session.get("inLocation")[1] == "in") {
-				readName = new SpeechSynthesisUtterance("You're in " + name);
-			}
-			else {
-				if (Session.get("unit") == "m") {
-					readName = new SpeechSynthesisUtterance("You're about " + Math.round(Session.get("inLocation")[2]) + "m from " + name);
-				}
-				else {
-					readName = new SpeechSynthesisUtterance("You're about " + Math.floor(Math.round(Session.get("inLocation")[2]*3.28)) + "ft from " + name);
-				}
-			}
-
-			window.speechSynthesis.speak(readName);
-		}
-
-		
-	},
-
-	'click #whatIsHere': function(event) {
-		event.preventDefault();
-
-		if (Session.get("inLocation") == null) {
-			window.speechSynthesis.speak(new SpeechSynthesisUtterance("There's lots to do off campus, but unfortunately I can't tell you all that much about it."));
-		}
-		else {
-			var loc = Locations.findOne({"name":Session.get("inLocation")[0].name});
-			//console.log(loc.function);
-			readFunction = new SpeechSynthesisUtterance(loc.function);
-
-			window.speechSynthesis.speak(readFunction);
-		}
+		Session.set("navigateTo",document.getElementById("endpoint").value);
+		Router.go('/navigate');
 	}
 });
